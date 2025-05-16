6602949a425a4ce5e2b22197c7fa7ff60a0a78f7
Breachfix <Ntihanirahojacques@gmail.com>
2025-05-16 05:33:10 -0700

    Fix: updated footer  folder

--- Frontend/src/components/footer/footer.jsx
+++ Frontend/src/components/footer/footer.jsx
@@ -1,6 +1,6 @@
 // Footer
 import { Link } from 'react-router-dom';
-import './Footer.css';
+import './footer.css';
 import SocialLinks from '../social-media/socials';
 
 export default function FooterComponent() {
--- Frontend/src/pages/faqs/Faq.jsx
+++ Frontend/src/pages/faqs/Faq.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-
+import './Faq.css';
 const faqs = [
   {
     question: "What is BreachFix Health?",
